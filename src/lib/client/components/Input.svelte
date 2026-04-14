b325908f6f8d39ec1bfa2ed810e0180ebd62b3c5
First Gemini PC <your.email@gmail.com>
2026-04-14 18:22:35 +0530

    Input partially developed

--- src/lib/client/components/Input.svelte
+++ src/lib/client/components/Input.svelte
@@ -48,50 +48,62 @@
 </script>
 
 <div
-  class="group relative flex appearance-none items-center divide-x-2 rounded border-2 not-last:mb-4
-        {isFocused ? 'divide-amber-400 border-amber-400' : ''}"
+  class="group relative appearance-none items-center rounded border-2 not-last:mb-4
+  {isFocused ? 'border-amber-400' : ''}"
 >
-  {#if prefixIcon}
-    <!-- svelte-ignore a11y_click_events_have_key_events -->
-    <!-- svelte-ignore a11y_no_static_element_interactions -->
-    <div
-      class="flex items-center justify-center self-stretch px-2 py-1
-            {isOnPrefixClickCallable ? 'cursor-pointer border-white hover:text-amber-400' : ''}"
-      onclick={onPrefixClick}
-    >
-      <span class="{icons[prefixIcon]} leading-none"></span>
-    </div>
-  {/if}
-
-  <div class="relative w-full px-2 py-1">
-    {#if label}
-      <span
-        class="absolute z-10 transition-all {labelActive
-          ? 'bottom-full translate-y-1/4 bg-black px-1 text-xs'
-          : 'bottom-1 translate-y-0 text-base'} {isFocused ? 'text-amber-400' : ''}"
+  <!-- Main Part -->
+  <div class="flex divide-x-2 {isFocused ? 'divide-amber-400' : ''}">
+    {#if prefixIcon}
+      <!-- svelte-ignore a11y_click_events_have_key_events -->
+      <!-- svelte-ignore a11y_no_static_element_interactions -->
+      <div
+        class="flex items-center justify-center self-stretch px-2 py-1
+              {isOnPrefixClickCallable ? 'cursor-pointer border-white hover:text-amber-400' : ''}"
+        onclick={onPrefixClick}
       >
-        {label}
-      </span>
+        <span class="{icons[prefixIcon]} leading-none"></span>
+      </div>
     {/if}
-    <input
-      {type}
-      bind:value
-      class="w-full bg-transparent outline-none"
-      onfocus={() => (isFocused = true)}
-      onblur={handleBlur}
-      oninput={handleInput}
-    />
+
+    <div class="relative w-full px-2 py-1">
+      {#if label}
+        <span
+          class="absolute z-10 transition-all {labelActive
+            ? 'bottom-full translate-y-1/4 scale-90 bg-black px-1 text-sm'
+            : 'bottom-1 translate-y-0 text-base'} {isFocused ? 'text-amber-400' : ''}"
+        >
+          {label}
+        </span>
+      {/if}
+      <input
+        {type}
+        bind:value
+        class="w-full bg-transparent outline-none"
+        onfocus={() => (isFocused = true)}
+        onblur={handleBlur}
+        oninput={handleInput}
+      />
+    </div>
+
+    {#if suffixIcon}
+      <!-- svelte-ignore a11y_click_events_have_key_events -->
+      <!-- svelte-ignore a11y_no_static_element_interactions -->
+      <div
+        class="flex items-center justify-center self-stretch px-2 py-1
+              {isOnSuffixClickCallable ? 'cursor-pointer border-white hover:text-amber-400' : ''}"
+        onclick={onSuffixClick}
+      >
+        <span class="{icons[suffixIcon]} leading-none"></span>
+      </div>
+    {/if}
   </div>
 
-  {#if suffixIcon}
-    <!-- svelte-ignore a11y_click_events_have_key_events -->
-    <!-- svelte-ignore a11y_no_static_element_interactions -->
-    <div
-      class="flex items-center justify-center self-stretch px-2 py-1
-            {isOnSuffixClickCallable ? 'cursor-pointer border-white hover:text-amber-400' : ''}"
-      onclick={onSuffixClick}
-    >
-      <span class="{icons[suffixIcon]} leading-none"></span>
-    </div>
-  {/if}
+  <!-- Options -->
+  <div
+    class="absolute z-20 w-full divide-y-2 divide-white/50 rounded-b border-2 border-white bg-gray-900 *:px-2 *:py-1"
+  >
+    <div>Test</div>
+    <div>Test</div>
+    <div>Test</div>
+  </div>
 </div>
